af2928f653314e335f8c7704fc78db47917dbedb
Niek Jan Ferwerda <33119394+NJFfromFrance@users.noreply.github.com>
2022-08-06 18:01:41 +0200

    Update auth.service.js
    I have used 'register' and 'login' in my api.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,14 +3,14 @@ import BaseHttpService from './base-http.service';
 
 export default class AuthService extends BaseHttpService {
   async signin(username, password) {
-    const result = await post(`${this.BASE_URL}/auth/signin`, { username, password });
+    const result = await post(`${this.BASE_URL}/auth/register`, { username, password });
     const accessToken = result.data.accessToken;
     this.saveToken(accessToken);
     return result.data.username;
   }
 
   async signup(username, password) {
-    await post(`${this.BASE_URL}/auth/signup`, { username, password });
+    await post(`${this.BASE_URL}/auth/login`, { username, password });
   }
 
   async signout() {
