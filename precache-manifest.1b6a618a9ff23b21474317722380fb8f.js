11a1eb99019ffcb8d84f8a9b49c67c88c2a294a2
NJFfromFrance <NJFfromFrance@users.noreply.github.com>
2022-08-06 16:35:45 +0000

    deploy: cb2ef93f4098d6b7a8fcc251af2f03dfe42996d0

---
+++ precache-manifest.1b6a618a9ff23b21474317722380fb8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721d6a10e464bbc44c9b3062f3ae5ca2",
+    "url": "https://NJFfromFrance.github.io/task-management-frontend /index.html"
+  },
+  {
+    "revision": "9bcdc73eef9ad60fa6a4",
+    "url": "https://NJFfromFrance.github.io/task-management-frontend /static/css/main.221589fe.chunk.css"
+  },
+  {
+    "revision": "521066b0d154cea2efe3",
+    "url": "https://NJFfromFrance.github.io/task-management-frontend /static/js/2.e495a87f.chunk.js"
+  },
+  {
+    "revision": "9bcdc73eef9ad60fa6a4",
+    "url": "https://NJFfromFrance.github.io/task-management-frontend /static/js/main.ba62e748.chunk.js"
+  },
+  {
+    "revision": "c20e7047b0f05b52ab61",
+    "url": "https://NJFfromFrance.github.io/task-management-frontend /static/js/runtime~main.99eed1d5.js"
+  }
+]);
